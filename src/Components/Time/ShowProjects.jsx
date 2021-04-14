603dd7999914a128a607e1ebfb60a5aaafc20d78
ashish8796 <ashishsaini.ak@gmail.com>
2021-04-14 16:22:35 +0530

    Get All tasks is done

--- src/Components/Time/ShowProjects.jsx
+++ src/Components/Time/ShowProjects.jsx
@@ -1,17 +1,63 @@
-import React from "react";
-import { shallowEqual, useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
+import { setTasksOfProject } from "../../store/projects/actions";
+import { setCurrentProject } from "../../store/time/actions";
+import { findItem } from "../../utils/findItem";
 
-export default function ShowProjects() {
-  const projects = useSelector((state) => state.time.projects, shallowEqual);
+export default function ShowProjects({
+  setAreProjectsVisible,
+  setIsLoading,
+  setIsError,
+  setIsTaskVisible,
+}) {
+  const projects = useSelector(
+    (state) => state.projects.projects,
+    shallowEqual
+  );
+  const dispatch = useDispatch();
+
+  const handleProjectClick = async (id) => {
+    setAreProjectsVisible(false);
+    const currentProject = findItem(id, projects);
+    dispatch(setCurrentProject(currentProject));
+
+    try {
+      setIsLoading(true);
+      const data = await dispatch(setTasksOfProject(id));
+
+      setIsLoading(false);
+      setIsTaskVisible(true);
+    } catch (error) {
+      console.log(error);
+      setIsError(true);
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      setIsLoading(false);
+      setIsError(false);
+    };
+  }, []);
 
   return (
     <ProjectsModal>
+      <h2>RECENT PROJECTS</h2>
       {projects.map((project) => (
-        <p>{project.name}</p>
+        <p key={project.id}>
+          <ProjectButton
+            onClick={(e) => {
+              handleProjectClick(project.id);
+            }}
+          >
+            {project.name}
+          </ProjectButton>
+        </p>
       ))}
     </ProjectsModal>
   );
 }
 
 const ProjectsModal = styled.div``;
+const ProjectButton = styled.button``;
