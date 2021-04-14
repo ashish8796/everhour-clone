2614c46d53731a34f6c655c54da932aeacbe35c0
smith <smithjacksj94@gmail.com>
2021-04-14 20:53:12 +0530

    fetched timerpage info

--- src/Components/Team/TimerBottombo.jsx
+++ src/Components/Team/TimerBottombo.jsx
@@ -1,42 +1,69 @@
 import React, { useEffect} from 'react'
 import { shallowEqual, useDispatch, useSelector } from 'react-redux'
 import {getTimerDetails} from "../../store/Team/action"
+import styled from "styled-components"
+
+const Th = styled.th`
+    color:#666666;
+    font-size:20px;
+    /* width:15%; */
+    /* margin-right:80px;
+    margin-bottom:100px; */
+`
+const Bigrow = styled.th`
+    width:350px;
+    font-size:22px;
+`
+
+const Bottom = styled.div`
+     width:85%;
+    margin:auto;
+    border:1px solid lightgray;
+    padding:0 40px;
+    color:#333333;
+
+
+`
 
 const TimerBottombo = () => {
 
-    const data = useSelector((state) => state.team.data)
+    const {data,isLoading,isError} = useSelector((state) => state.team,shallowEqual)
+    console.log(data)
+    console.log("hi")
     const dispatch  = useDispatch()
 
     useEffect(() => {
         dispatch(getTimerDetails())
     },[dispatch])
 
-    return (
-        <div>
-            <div>{data.length === 0 && "No Activity"}</div>
+    return isLoading?<div>...Loading</div>:
+        isError?<div>Something went wrong</div>:
+        (
+        <Bottom>
+            <div>{data.length === 0 && <h3>No Activity</h3>}</div>
             {data?.map(info => {
                 return (
                     <table>
                         <thead>
                             <tr>
-                                <th>Activity</th>
-                                <th>Time</th>
-                                <th>Commit</th>
-                                <th>Total</th>
+                                <Bigrow>Activity</Bigrow>
+                                <Th>Time</Th>
+                                <Th>Commit</Th>
+                                <Th>Total</Th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <th>{info.username} {info.status}</th>
-                                <th>{info.duration}</th>
-                                <th>{info.task.name}</th>
-                                <th>{info.time.total}</th>
+                                <td>{info.user.name} {info.status}</td>
+                                <td>{info.startedAt}</td>
+                                <td>{info.comment}</td>
+                                <td>{info.duration}</td>
                             </tr>
                         </tbody>
                     </table>
                 )
             })}
-        </div>
+        </Bottom>
     )
 }
 
