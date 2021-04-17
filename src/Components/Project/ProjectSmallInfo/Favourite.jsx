4d6be05e80a043527617f59ee6309f5d00b5528c
ashish8796 <ashishsaini.ak@gmail.com>
2021-04-17 23:25:03 +0530

    Project page css is done

---
+++ src/Components/Project/ProjectSmallInfo/Favourite.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+export default function Favourite({ setFavourite }) {
+  const [toggleFavourite, setToggleFavourite] = useState(false);
+
+  const handleToggleFavourite = (e) => {
+    e.preventDefault();
+    setFavourite((prev) => !prev);
+    setToggleFavourite(!toggleFavourite);
+  };
+
+  return (
+    <svg width="19" height="18" onClick={handleToggleFavourite}>
+      <path
+        fill={toggleFavourite ? "wheat" : "#fdfdfd"}
+        fill-rule="evenodd"
+        stroke="#dad8d8"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M9.425 14.538l4.562 2.396a.576.576 0 00.835-.604l-.87-5.08 3.692-3.595a.576.576 0 00-.316-.985l-5.098-.743L9.926 1.32a.576.576 0 00-1.031 0L6.59 5.927l-5.098.743a.576.576 0 00-.316.985l3.692 3.594-.87 5.08a.576.576 0 00.835.605l4.59-2.396z"
+      />
+    </svg>
+  );
+}
--- src/Components/Project/ProjectSmallInfo/ProjectSmallInfo.jsx
+++ src/Components/Project/ProjectSmallInfo/ProjectSmallInfo.jsx
@@ -1,110 +1,129 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { Redirect, Route, useHistory } from 'react-router';
-import { Link } from 'react-router-dom';
-import { deleteProject } from '../../../store/projects/actions';
-import { loadData } from '../../../utils/localStorage';
-import { ProjectTask } from '../Task/ProjectTask';
-import styles from './ProjectSmallInfo.module.css';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect, useHistory } from "react-router";
+import { deleteProject } from "../../../store/projects/actions";
+import { findItem } from "../../../utils/findItem";
+import { loadData } from "../../../utils/localStorage";
+import CreateUserImg from "./CreateUserImg";
+import Favourite from "./Favourite";
+import styles from "./ProjectSmallInfo.module.css";
 
-const ProjectSmallInfo = ({name, createdAt, id}) => {
-    const [billingVisible, setBillingVisible] = React.useState(false);
-    const [redProjectTask, setRedProjectTask] = React.useState(false);
-    const [favourite, setFavourite] =React.useState(false)
-    let history = useHistory();
-    const [redURL, setRedURL] = React.useState("");
-    const dispatch = useDispatch();
-    const imgUrl = loadData("avatar");
+const ProjectSmallInfo = ({ project }) => {
+  const { name, createdAt, id, users } = project;
+  const [billingVisible, setBillingVisible] = React.useState(false);
+  const [redProjectTask, setRedProjectTask] = React.useState(false);
+  const [favourite, setFavourite] = React.useState(false);
+  let history = useHistory();
+  const [redURL, setRedURL] = React.useState("");
+  const dispatch = useDispatch();
+  const imgUrl = loadData("avatar");
+  const { allUsers } = useSelector((state) => state.user);
 
-    const billingSmallInfo = (id) => {
-        console.log(id);
-        setBillingVisible(true)
+  const billingSmallInfo = (id) => {
+    console.log(id);
+    setBillingVisible(true);
+  };
+  const handleDeleteSmallInfo = (id) => {
+    const idType = id.slice(0, 2);
+    if (idType === "ev") {
+      console.log("Suceess");
+      dispatch(deleteProject(id));
+    } else {
+      alert("This is Created from Github so from here it is not possible");
     }
-    const handleDeleteSmallInfo = (id) => {
-        const idType = id.slice(0,2)
-        if(idType === "ev"){
-            console.log("Suceess");
-            dispatch(deleteProject(id))
-        } else {
-            alert("This is Created from Github so from here it is not possible")
-        }
-    }
-    const handleBillingSave = () => {
-        setBillingVisible(false)
-    }
-    const handleProjectTask =()=>{
-        setRedProjectTask(true);
-        history.push("/projects");
-        setRedURL("/task/"+id);
-    }
-    const handleFavourite = (e, id) => {
-        setFavourite(!favourite);
-        console.log(favourite, id)
-    }
-    return !billingVisible && !redProjectTask?(
-        <div className={styles.divPartMain}>
-            <div>
-                <button onClick={()=>handleProjectTask(id)}><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt=""/></button>
-            </div>
-            <div>
-                <h6>{name}</h6>
-                <p>{createdAt}</p>
-            </div>
-            <div title="Favourite">
-                <input type="checkbox" onChange={(e)=>handleFavourite(e, id)} />
-                <label htmlFor="">Favourite</label>
-            </div>
-            <div title="Delete">
-                <button onClick={()=>handleDeleteSmallInfo(id)}>Delete</button>
-            </div>
-            <div className={styles.divPartMember}>
-                <img style={{width: "28px"}} src={imgUrl} alt=""/>
-            </div>
-            <div>
-                <button onClick={()=>billingSmallInfo(id)}>$</button>
-            </div>
-        </div>
-    ):!redProjectTask?(
-        <div className={styles.divPartBilling}>
-            <h3>Project Billing</h3>
-            <div className={styles.divPartBillingFlex}>
-                <div>
-                    <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt=""/>
-                </div>
-                <div>
-                    <h6>{name}</h6>
-                    <p>{createdAt}</p>
-                </div>
-            </div>
-            <label htmlFor="">Billing</label>
-            <div className={styles.divPartBillingInput}>
-                <select name="" id="" >
-                    <option value="Non-Billible">Non-Billible</option>
-                    <option value="Hourly(Project Hours)">Hourly(Project Hours)</option>
-                    <option value="Hourly(Member Rate)">Hourly(Member Rate)</option>
-                    <option value="Fixed Fee">Fixed Fee</option>
-                </select>
-                <input type="number" placeholder="$ Amount" />
-            </div>
-            <label htmlFor="">Budget</label>
-            <div className={styles.divPartBillingInput}>
-                <select name="" id="" >
-                    <option value="No-Budget">No-Budget</option>
-                    <option value="Total Project Hours">Total Project Hours</option>
-                    <option value="Total Project Fees">Total Project Fees</option>
-                    <option value="Fixed Fee">Fixed Fee</option>
-                </select>
-                <input type="number" placeholder="$ Budget" />
-            </div>
-            <button onClick={()=>handleBillingSave(id)}>Save</button>
-        </div>
-    ):(
-        <div><>
-            {/* {console.log(redURL)} */}
-            <Redirect to={redURL} />
-            </>
-        </div>
-    )
-}
+  };
 
-export {ProjectSmallInfo}
+  const handleBillingSave = () => {
+    setBillingVisible(false);
+  };
+
+  const handleProjectTask = () => {
+    setRedProjectTask(true);
+    history.push("/projects");
+    setRedURL("/task/" + id);
+  };
+
+  return !billingVisible && !redProjectTask ? (
+    <div className={styles.divPartMain}>
+      <section className={`${styles.projectDetailsCont} flex`}>
+        <div className={styles.logo}>
+          <img src="/assets/Everhour_logo.svg" alt="logo" />
+        </div>
+
+        <div className={`${styles.taskName}`}>
+          <h6 onClick={() => handleProjectTask(id)}>{name}</h6>
+          <p>{createdAt}</p>
+        </div>
+
+        <div className={styles.favourite} title="Favourite">
+          <Favourite setFavourite={setFavourite} />
+        </div>
+      </section>
+
+      <section className={`${styles.projectOption} grid`}>
+        <div className={styles.divPartMember}>
+          {allUsers.length > 0 &&
+            users.map((userId) => (
+              <CreateUserImg key={userId} user={findItem(userId, allUsers)} />
+            ))}
+        </div>
+
+        <div className={`${styles.dollar} `}>
+          <button onClick={() => billingSmallInfo(id)}>$</button>
+        </div>
+
+        <div title="Delete" className={`${styles.delete} flex align-center`}>
+          <button onClick={() => handleDeleteSmallInfo(id)}>
+            <img src="/assets/delete.svg" alt="delete" />
+          </button>
+        </div>
+      </section>
+    </div>
+  ) : !redProjectTask ? (
+    <div className={styles.divPartBilling}>
+      <h3>Project Billing</h3>
+      <div className={styles.divPartBillingFlex}>
+        <div>
+          <img
+            src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+            alt=""
+          />
+        </div>
+        <div>
+          <h6>{name}</h6>
+          <p>{createdAt}</p>
+        </div>
+      </div>
+      <label htmlFor="">Billing</label>
+      <div className={styles.divPartBillingInput}>
+        <select name="" id="">
+          <option value="Non-Billible">Non-Billible</option>
+          <option value="Hourly(Project Hours)">Hourly(Project Hours)</option>
+          <option value="Hourly(Member Rate)">Hourly(Member Rate)</option>
+          <option value="Fixed Fee">Fixed Fee</option>
+        </select>
+        <input type="number" placeholder="$ Amount" />
+      </div>
+      <label htmlFor="">Budget</label>
+      <div className={styles.divPartBillingInput}>
+        <select name="" id="">
+          <option value="No-Budget">No-Budget</option>
+          <option value="Total Project Hours">Total Project Hours</option>
+          <option value="Total Project Fees">Total Project Fees</option>
+          <option value="Fixed Fee">Fixed Fee</option>
+        </select>
+        <input type="number" placeholder="$ Budget" />
+      </div>
+      <button onClick={() => handleBillingSave(id)}>Save</button>
+    </div>
+  ) : (
+    <div>
+      <>
+        {/* {console.log(redURL)} */}
+        <Redirect to={redURL} />
+      </>
+    </div>
+  );
+};
+
+export { ProjectSmallInfo };
